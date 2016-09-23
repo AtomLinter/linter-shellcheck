01c3c5ee8e0132d49b22da9a457227e21e95c7d6
Landon Abney <Landonabney@gmail.com>
2016-09-23 10:23:01 -0700

    Add basic specs
    Add some specs testing that the linter is operating properly. Currently no
    options are being tested.

---
+++ spec/linter-shellcheck-spec.js
@@ -0,0 +1,43 @@
+'use babel';
+
+import * as path from 'path';
+
+const cleanPath = path.join(__dirname, 'fixtures', 'clean.sh');
+const badPath = path.join(__dirname, 'fixtures', 'bad.sh');
+
+describe('The ShellCheck provider for Linter', () => {
+  const lint = require(path.join('..', 'lib', 'main.coffee')).provideLinter().lint;
+
+  beforeEach(() => {
+    atom.workspace.destroyActivePaneItem();
+
+    waitsForPromise(() =>
+      Promise.all([
+        atom.packages.activatePackage('linter-shellcheck'),
+      ])
+    );
+  });
+
+  it('finds nothing wrong with a valid file', () => {
+    waitsForPromise(() =>
+      atom.workspace.open(cleanPath).then(editor => lint(editor)).then((messages) => {
+        expect(messages.length).toBe(0);
+      })
+    );
+  });
+
+  it('handles messages from ShellCheck', () => {
+    const expectedMsg = 'Tips depend on target shell and yours is unknown. Add a shebang. ' +
+      '[<a href="https://github.com/koalaman/shellcheck/wiki/SC2148">SC2148</a>]';
+    waitsForPromise(() =>
+      atom.workspace.open(badPath).then(editor => lint(editor)).then((messages) => {
+        expect(messages.length).toBe(1);
+        expect(messages[0].type).toBe('error');
+        expect(messages[0].text).not.toBeDefined();
+        expect(messages[0].html).toBe(expectedMsg);
+        expect(messages[0].filePath).toBe(badPath);
+        expect(messages[0].range).toEqual([[0, 0], [0, 4]]);
+      })
+    );
+  });
+});

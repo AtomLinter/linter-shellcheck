9f2ac4d2cf945fc9c58b2bada84e4235bb7d17ee
Ted Behling <tbehling@github.com>
2018-08-23 11:35:01 -0400

    Specs for useProjectCwd

--- spec/linter-shellcheck-spec.js
+++ spec/linter-shellcheck-spec.js
@@ -8,6 +8,8 @@ const { lint } = require('../lib/main.js').provideLinter();
 
 const cleanPath = path.join(__dirname, 'fixtures', 'clean.sh');
 const badPath = path.join(__dirname, 'fixtures', 'bad.sh');
+const sourceFileRelativePath = path.join(__dirname, 'fixtures', 'source_directive', 'file_relative.sh');
+const sourceProjectRelativePath = path.join(__dirname, 'fixtures', 'source_directive', 'project_relative.sh');
 
 describe('The ShellCheck provider for Linter', () => {
   beforeEach(async () => {
@@ -43,5 +45,42 @@ describe('The ShellCheck provider for Linter', () => {
     expect(messages[0].html).toBe(expectedMsg);
     expect(messages[0].filePath).toBe(badPath);
     expect(messages[0].range).toEqual([[0, 0], [0, 4]]);
+  });
+
+  describe('implements useProjectCwd and', () => {
+    beforeEach(async () => {
+      atom.config.set('linter-shellcheck.userParameters', '-x');
+      atom.config.set('linter-shellcheck.enableNotice', true);
+    });
+
+    it('uses file-relative source= directives by default', async () => {
+      atom.config.set('linter-shellcheck.useProjectCwd', false);
+      const editor = await atom.workspace.open(sourceFileRelativePath);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(0);
+    });
+
+    it('errors for file-relative source= path with useProjectCwd = true', async () => {
+      atom.config.set('linter-shellcheck.useProjectCwd', true);
+      const editor = await atom.workspace.open(sourceFileRelativePath);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(1);
+      expect(messages[0].html).toMatch(/openBinaryFile: does not exist/);
+    });
+
+    it('uses project-relative source= directives via setting (based at fixtures/)', async () => {
+      atom.config.set('linter-shellcheck.useProjectCwd', true);
+      const editor = await atom.workspace.open(sourceProjectRelativePath);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(0);
+    });
+
+    it('errors for project-relative source= path with useProjectCwd = false (based at fixtures/)', async () => {
+      atom.config.set('linter-shellcheck.useProjectCwd', false);
+      const editor = await atom.workspace.open(sourceProjectRelativePath);
+      const messages = await lint(editor);
+      expect(messages.length).toBe(1);
+      expect(messages[0].html).toMatch(/openBinaryFile: does not exist/);
+    });
   });
 });
